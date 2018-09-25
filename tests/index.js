8a67205ca5539a67bd319b90f6baf7d780b1d5b1
Willem Olding <willemolding@gmail.com>
2018-09-25 16:05:05 +1000

    adds javascript wrapper to functions can be called from test

---
+++ tests/index.js
@@ -0,0 +1,11 @@
+var asmjson = require("..");
+var test = require('tape');
+
+test('timing test', function (t) {
+    t.plan(2);
+
+    t.equal(typeof Date.now, 'function');
+    var start = Date.now();
+
+    t.equal(0,0)
+});
