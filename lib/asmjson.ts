4e3fc47da8ae4b410906b100c0a4b4267ce8f135
Willem Olding <willemolding@gmail.com>
2018-09-26 14:00:09 +1000

    adds successfully parsing an object!

--- lib/asmjson.ts
+++ lib/asmjson.ts
@@ -23,49 +23,122 @@ enum State {
 	Start = 0,
 	ObjectInitial = 1,
 	MemberKey = 2,
-	KeyValueDelimiter = 3,
-	MemberValue = 4,
-	MemberDelimiter = 5,
-	ObjectFinish = 6,
-	Finish = 7
+	PreDelimiter = 3,
+	PostDelimiter = 4,
+	MemberValue = 5,
+	PostMember = 6,
+	ObjectFinish = 7,
+	Finish = 8
 }
 
 const whitespace: Array<string> = [" ", "\t", "\n", "\r"];
-// state functions
 
-function start(): void {
+var state: State;
+var i: i32;
+var stringBuffer: string;
+
+/*----------  State Functions  ----------*/
+
+function start<HandlerType extends Handler>(c: string, handler: HandlerType): void {
+	if(c == `{`) {
+		state = State.ObjectInitial;
+		handler.onObjectStart();
+	}
+}
+
+function objectInitial<HandlerType extends Handler>(c: string, handler: HandlerType): void {
+	if(c == `}`) {
+		state = State.ObjectInitial;
+		handler.onObjectEnd();
+	} else if (c == `"`) {
+		state = State.MemberKey;
+	}
+}
+
+function memberKey<HandlerType extends Handler>(c: string, handler: HandlerType): void {
+	if(c == `"`) {
+		state = State.PreDelimiter;
+		handler.onKey(stringBuffer);
+		stringBuffer = "";
+	} else {
+		stringBuffer += c;
+	}
+}
+
+function preDelimiter<HandlerType extends Handler>(c: string, handler: HandlerType): void {
+	if(c == `:`) {
+		state = State.PostDelimiter;
+	}
+}
+
+function postDelimiter<HandlerType extends Handler>(c: string, handler: HandlerType): void {
+	if(c == `"`) { // start of string
+		state = State.MemberValue;
+	}
+}
+
+// TODO: modifiy to handle values other than strings...
+function memberValue<HandlerType extends Handler>(c: string, handler: HandlerType): void {
+	if(c == `"`) { // end of string
+		state = State.PostMember;
+		handler.onString(stringBuffer);
+		stringBuffer = "";
+	} else {
+		stringBuffer += c;
+	}
+}
+
+function postMember<HandlerType extends Handler>(c: string, handler: HandlerType): void {
+	if(c == `,`) {
+		state = State.PostDelimiter
+	} else if (c == `}`) {
+		state = State.ObjectFinish;
+		handler.onObjectEnd();
+	}
+}
+
+function objectFinish<HandlerType extends Handler>(c: string, handler: HandlerType): void {
 
 }
 
-function objectInitial(): void {
+function finish<HandlerType extends Handler>(c: string, handler: HandlerType): void {
 
 }
 
-function memberKey(): void {
 
-}
-
-function keyValueDelimiter(): void {
-
-}
-
-function memberValue(): void {
-
-}
-
-function memberDelimiter(): void {
-
-}
-
-function objectFinish(): void {
-
-}
-
-function finish(): void {
-
-}
 
 export function parseString<HandlerType extends Handler>(jsonString: string, handler: HandlerType): i32 {
-	let state: State = State.Start;
-	return 0;
+	// initialization
+	state = State.Start;
+	stringBuffer = "";
+	i = 0;
+
+	for(; i < jsonString.length; ++i) {
+		let c: string = jsonString[i];
+		switch(state) {
+			case State.Start:
+				start<HandlerType>(c, handler);
+				break;
+			case State.ObjectInitial:
+				objectInitial<HandlerType>(c, handler);
+				break;
+			case State.MemberKey:
+				memberKey<HandlerType>(c, handler);
+				break;
+			case State.PreDelimiter:
+				preDelimiter<HandlerType>(c, handler);
+				break;
+			case State.PostDelimiter:
+				postDelimiter<HandlerType>(c, handler);
+				break;
+			case State.MemberValue:
+				memberValue<HandlerType>(c, handler);
+				break;
+			case State.PostMember:
+				postMember<HandlerType>(c, handler);
+				break;
+		}
+	}
+
+	return -1;
 }
